85cd28ddbeeccb66af058db934f36b7e4892d7a3
swissarmytowel <vetal4post@gmail.com>
2017-10-12 17:26:28 +0300

    Fixed URLError reason printing issue. Added parsing exception. Added parsing method by tag

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,20 +1,33 @@
-from bs4 import BeautifulSoup
-from socket import error as SocketError
+from socket import error as socket_error
 from urllib.error import URLError
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+class ParserError(Exception):
+    def __init__(self, *args):
+        Exception.__init__(self, *args)
+        self.__message = "Parsing error occured"
+
+    def what(self):
+        return self.__message
 
 
 class WebCrawler(object):
     """Class implementing web crawler.
        Crawls web pages for specific data retaining. """
 
-    __page_html = ""
-
     def __init__(self, url: str):
         # Init url and append 'https://' if absent
         self.__url = url
-        if self.__url[:7] != "https://":
+        if self.__url.count("https://") > 1:
+            self.__url[8:].replace("https://", "")
+        if not self.__url.startswith("https://"):
             self.__url = "https://" + self.__url
+
+        self.__page_html = str()
+        self.__retained_items_list = list()
+
         # Initialize connection, retrieve html page
         self.retrieve_html()
         self.__page_soup = BeautifulSoup(self.__page_html, "html.parser")
@@ -26,15 +39,25 @@ class WebCrawler(object):
             self.__page_html = url_client.read()
             url_client.close()
         except URLError as e:
-            print("Url error occurred: " + e.reason())
+            print("Url error occurred: " + str(e.reason))
         except ValueError:
             print("Unknown url type: \'" + self.__url + "'")
-        except SocketError as e:
+        except socket_error as e:
             print("Socket error has occurred: " + e.errno + " in " + e.filename)
 
-if __file__ == "__main__":
-    c = WebCrawler("stackoverflow.com")
+    def parse_html(self, tag: str):
+        self.__retained_items_list = self.__page_soup.findAll(tag)
+        if self.__retained_items_list is None:
+            raise ParserError
 
+    def get_retained_items(self):
+        return self.__retained_items_list
 
-
+if __name__ == "__main__":
+    tmp = WebCrawler("https://www.python.org")
+    try:
+        tmp.parse_html(tag='img')
+    except ParserError as e:
+        print(e.what())
+    print(len(tmp.get_retained_items()))
 
