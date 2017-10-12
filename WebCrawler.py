3bf450fb84d374e6c6ee58570df8dd7f8708a5ae
swissarmytowel <vetal4post@gmail.com>
2017-10-12 20:37:11 +0300

    Added message parameter to exception and missing docstrings

--- WebCrawler.py
+++ WebCrawler.py
@@ -5,9 +5,13 @@ from bs4 import BeautifulSoup
 
 
 class ParserError(Exception):
-    def __init__(self):
+    """Custom HTML parser exception"""
+    def __init__(self, message=None):
         Exception.__init__(self)
-        self.__message = "Parsing html error occurred"
+        if message is not None:
+            self.__message = message
+        else:
+            self.__message = "Parsing html error occurred"
 
     @property
     def what(self):
@@ -17,19 +21,21 @@ class ParserError(Exception):
 class WebCrawler(object):
     """Class implementing web crawler.
        Crawls web pages for specific data retaining. """
-
     def __init__(self, url: str):
-        # Init url and append 'https://' if absent
+        """
+        WebCrawler constructor
+        :param url: address of web page to be crawled
+        """
+        # Initialize url and append 'https://' if absent
         self.__url = url
         if self.__url.count("https://") > 1:
             self.__url[8:].replace("https://", "")
         if not self.__url.startswith("https://"):
             self.__url = "https://" + self.__url
 
+        # Initialize connection, retrieve html page
         self.__page_html = str()
         self.__retained_items_list = list()
-
-        # Initialize connection, retrieve html page
         self.retrieve_html()
         self.__page_soup = BeautifulSoup(self.__page_html, "html.parser")
 
@@ -46,14 +52,21 @@ class WebCrawler(object):
         except socket_error as socket_exception:
             print("Socket error has occurred: " + socket_exception.errno + " in " + socket_exception.filename)
 
-    def parse_html(self, tag: str):
+    def parse_html(self, tag: str, sub_tag: str = None):
+        """
+        Parses HTML for tag occurrences and then gets substring by sub tag
+        :param tag: tag for initial splitting
+        :param sub_tag: sub tag for retrieving parts of interest
+        """
         self.__retained_items_list = self.__page_soup.findAll(tag)
         if self.__retained_items_list is None:
             raise ParserError
-        self.__retained_items_list = [item.get("src")
-                                      if item.get("src").startswith(self.__url)
-                                      else urljoin(self.__url, item.get("src"))
-                                      for item in self.__retained_items_list]
+        # Retrieve part of interest, if :param sub_tag was specified
+        if sub_tag is not None:
+            self.__retained_items_list = [item.get(sub_tag)
+                                          if item.get(sub_tag).startswith(self.__url)
+                                          else urljoin(self.__url, item.get(sub_tag))
+                                          for item in self.__retained_items_list]
 
     @property
     def get_retained_items(self) -> list:
@@ -61,15 +74,18 @@ class WebCrawler(object):
 
 
 if __name__ == "__main__":
-    test_url = "https://www.python.org"
-    test_tag = "img"
 
+    # Use case
+
+    test_url = "python.org"
+    test_tag = "img"
+    test_sub_tag = 'src'
     test_spider = WebCrawler(test_url)
     try:
-        test_spider.parse_html(test_tag)
+        test_spider.parse_html(test_tag, test_sub_tag)
     except ParserError as e:
         print(e.what)
 
     print("Found " + str(len(test_spider.get_retained_items)) + " items from \"" + test_url +
           "\" by tag \"" + test_tag + "\"")
-    print(test_spider.get_retained_items)
+    print("\n".join(test_spider.get_retained_items))
